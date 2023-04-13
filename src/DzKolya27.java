263cccf8d9914e60d93c316005637f6f9d838ac3
Kolya-n9t <teplyakov.itsmart@gmail.com>
2023-04-13 14:40:27 +0300

    DzKolya

---
+++ src/DzKolya27.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+public class DzKolya27{
+    public static void main(String[] args) {
+        String st = "Counting the number of specific words";
+        String[] words = st.split(" ");
+        HashMap<String,Integer> keyValue = new HashMap<String,Integer>();
+        for (int i=0; i<= words.length-1; i++) {
+            if (keyValue.containsKey(words[i])) {
+                int counter = keyValue.get(words[i]);
+                keyValue.put(words[i], counter+1);
+            }
+            else {
+                keyValue.put(words[i], 1);
+            }
+        }
+        System.out.println(keyValue);
+    }
+}
